318554e146b3fc34d981186ec5140e151e7f8f03
elodloic <95284344+elodloic@users.noreply.github.com>
2024-10-09 15:40:20 +0200

    PWA implementation
    Made changes to turn the app in to a Progressive Web App:
    -updated manifest.json
    -registered service worker
    -local events storage

--- src/api.js
+++ src/api.js
@@ -60,6 +60,11 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    return events ? JSON.parse(events) : [];
+  }
+
   const token = await getAccessToken();
 
   if (token) {
@@ -71,6 +76,7 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
