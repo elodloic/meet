9d6bd0fe2aad6013c6df4ac7aad06ae6bd7cf08b
elodloic <95284344+elodloic@users.noreply.github.com>
2024-10-10 19:20:46 +0200

    Pie- and Scattered charts
    Added charts to visualise app data.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import NumberOfEvents from "./components/NumberOfEvents";
 import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
 import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
+import CityEventsChart from "./components/CityEventsChart";
+import EventGenresChart from "./components/EventGenresChart";
 
 import "./App.css";
 
@@ -57,6 +59,11 @@ const App = () => {
       />
       <div className="error-container">
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+      </div>
+
+      <div className="charts-container">
+        <CityEventsChart allLocations={allLocations} events={events} />
+        <EventGenresChart events={events} />
       </div>
       <EventList events={events} />
     </div>
